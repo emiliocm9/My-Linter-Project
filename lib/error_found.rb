efaaf9c4886b959ac0d725c2948744a40bf03758
emiliocm9 <emilio.contreras97@gmail.com>
2020-06-29 17:02:56 -0500

    better

--- lib/error_found.rb
+++ lib/error_found.rb
@@ -1,9 +1,9 @@
 class ErrorFound
-  attr_accessor :indent, :open_tag, :close_tag, :angle_bracket
+  attr_accessor :ident, :open_tag, :close_tag, :angle_bracket
 
   def initialize
     @angle_bracket = []
-    @indent = []
+    @ident = []
     @open_tag = Hash.new(0)
     @close_tag = Hash.new(0)
   end
